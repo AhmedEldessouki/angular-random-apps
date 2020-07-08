3d581849b20d9fa7c0c1437d18138e91ba21ef5e
Nemo <Nemo@NEMO>
2020-07-08 11:30:54 +0300

    setting up

--- tic-tac-toe/src/app/app.module.ts
+++ tic-tac-toe/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SquareComponent } from './square/square.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SquareComponent
   ],
   imports: [
     BrowserModule,
---
+++ tic-tac-toe/src/app/square/square.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  template: `
+    <p>
+      square works!
+    </p>
+  `,
+  styles: [],
+})
+export class SquareComponent {}
